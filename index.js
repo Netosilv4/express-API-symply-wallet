73d69ca227b0786eb37bf74390a2206e5a6c70f8
netosilv4 <netosilv44445@gmail.com>
2021-07-21 15:11:37 -0300

    Installing cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 
+const cors = require('cors')
+
 const route = require('./routes/routes')
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send({ message: "Tudo 10" })
