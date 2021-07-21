8ddbc640779ad2765490ba3979823f86cd5d9a52
netosilv4 <netosilv44445@gmail.com>
2021-07-21 15:24:13 -0300

    Removing cors

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express')
 
-const cors = require('cors')
-
 const route = require('./routes/routes')
 
 const app = express()
 
 app.use(express.json())
-
-app.use(cors())
 
 app.get('/', (req, res) => {
   res.send({ message: "Tudo 10" })
