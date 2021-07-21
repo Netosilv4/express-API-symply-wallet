c39d7cbce479a99c3e20b7c3142d37be1971a528
netosilv4 <netosilv44445@gmail.com>
2021-07-20 23:09:34 -0300

    Testing 5...

--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,12 @@
 const pg = require('pg')
 
 const client = new pg.Client({
-  user: process.env.USER_KEY,
-  host: process.env.HOST_KEY,
-  database: process.env.DATABASE_KEY,
-  password: process.env.PASSWORD_KEY,
+  user: 'bbghysbgvfomha',
+  host: 'ec2-52-1-20-236.compute-1.amazonaws.com',
+  database: 'dejloahcvn3abg',
+  password: 'f7479f594e3b632184d75883f22bf868709374864cd5d15e1a9972f8aab2018b',
   port: process.env.PORT,
+  ssl: { rejectUnauthorized: false }
 })
 
 client.connect();
