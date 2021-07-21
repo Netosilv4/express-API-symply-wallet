9c2c315d95084874ff62fc9c989efcc4f5a962e0
netosilv4 <netosilv44445@gmail.com>
2021-07-20 23:20:20 -0300

    Testing 8

--- models/connection.js
+++ models/connection.js
@@ -1,15 +1,5 @@
-const pg = require('pg')
-
-const client = new pg.Client({
-  user: 'bbghysbgvfomha',
-  host: 'ec2-52-1-20-236.compute-1.amazonaws.com',
-  database: 'dejloahcvn3abg',
-  password: 'f7479f594e3b632184d75883f22bf868709374864cd5d15e1a9972f8aab2018b',
-  port: process.env.PORT,
-  ssl: { rejectUnauthorized: false }
-})
-
-client.connect();
+var pgp = require('pg-promise')(/* options */)
+var client = pgp('postgres://bbghysbgvfomha:f7479f594e3b632184d75883f22bf868709374864cd5d15e1a9972f8aab2018b@ec2-52-1-20-236.compute-1.amazonaws.com:5432/dejloahcvn3abg')
 
 module.exports = {
   client,
